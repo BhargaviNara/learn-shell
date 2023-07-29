abca35c21ffba207275e1f985839b6ade81b13d5
Bhargavi Nara <bhargavinara.informatica@gmail.com>
2023-07-29 14:44:23 +1200

    script

--- echo.sh
+++ echo.sh
@@ -1,8 +1,14 @@
 echo ">>>>>>>>>hello wworld<<<<<<<<<<"
+#red-32
+#green-32
+#yellow-33
+#blue-34
+#magenta-35
+#cyan-36
 
-echo -e "\e[31--HelloWorld--\e[0m"
-echo -e "\e[32--HelloWorld--\e[0m"
-echo -e "\e[33--HelloWorld--\e[0m"
-echo -e "\e[34--HelloWorld--\e[0m"
-echo -e "\e[35--HelloWorld--\e[0m"
-echo -e "\e[36--HelloWorld--\e[0m"
+echo -e "\e[31mHelloWorld\e[0m"
+echo -e "\e[32mHelloWorld\e[0m"
+echo -e "\e[33mHelloWorld\e[0m"
+echo -e "\e[34mHelloWorld\e[0m"
+echo -e "\e[35mHelloWorld\e[0m"
+echo -e "\e[36mHelloWorld\e[0m"
