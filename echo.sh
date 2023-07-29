99fdf1fbd693b22fc7bd3fb8d1ebe9786ccd70e6
Bhargavi Nara <bhargavinara.informatica@gmail.com>
2023-07-29 14:39:04 +1200

    script

---
+++ echo.sh
@@ -0,0 +1,8 @@
+echo ">>>>>>>>>hello wworld<<<<<<<<<<"
+
+echo -e "\e[31--HelloWorld--\e[0m"
+echo -e "\e[32--HelloWorld--\e[0m"
+echo -e "\e[33--HelloWorld--\e[0m"
+echo -e "\e[34--HelloWorld--\e[0m"
+echo -e "\e[35--HelloWorld--\e[0m"
+echo -e "\e[36--HelloWorld--\e[0m"
